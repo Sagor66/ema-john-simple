b2699f382d63ab6c4f3e58bf8f6a135377ff733e
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-03-30 03:17:11 +0600

    saved cart to local storage

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -3,17 +3,17 @@ import "./Cart.css";
 
 const Cart = ({ cart }) => {
   // const {cart} = props
-  console.log(cart)
-  let totalPrice = 0;
-  let totalShipping = 0
   
+  let totalPrice = 0;
+  let totalShipping = 0;
+
   for (const product of cart) {
     totalPrice = totalPrice + product.price;
-    totalShipping = totalShipping + product.shipping
+    totalShipping = totalShipping + product.shipping;
   }
 
-  const tax = (totalPrice*7)/100
-  const grandTotal = totalPrice + totalShipping + tax
+  const tax = (totalPrice * 7) / 100;
+  const grandTotal = totalPrice + totalShipping + tax;
   return (
     <div className="cart">
       <h4>Order Summary</h4>
