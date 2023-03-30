09cccca06a184fb45488272578dc427130932051
Sagor66 <48826631+Sagor66@users.noreply.github.com>
2023-03-30 14:54:22 +0600

    show data from cart

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -29,14 +29,27 @@ const Shop = () => {
         savedCart.push(addedProduct)
       }
 
-      console.log(addedProduct);
+      
     }
     // step 5: set the cart
     setCart(savedCart)
   }, [products]);
 
   const handleAddToCart = (product) => {
-    const newCart = [...cart, product];
+    // const newCart = [...cart, product];
+    let newCart = [];
+    const exists = cart.find(pd => pd.id === product.id)
+    
+    if (!exists) {
+      product.quantity = 1;
+      newCart = [...cart, product]
+
+    } else {
+      exists.quantity = exists.quantity + 1
+      const remaining = cart.filter(pd => pd.id !== product.id);
+      newCart = [...remaining, exists];
+      
+    }
     setCart(newCart);
     addToDb(product.id);
   };
